a48ddb2779fd01c39db2dc822c2178145c6fb306
deankp <dperez0689@gmail.com>
2022-05-04 21:45:38 -0500

    updated index.js

--- public/index.js
+++ public/index.js
@@ -4,6 +4,8 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    const response = await fetch('https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=266c1408c46645f986cc60ae8c606ea4')
+    const result = await response.json()
 }
 
 main()
